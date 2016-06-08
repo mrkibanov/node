181a8828a3a607898a988e7af2a1e389c85f8741
Vadym Kibanov <dev.kibanov@gmail.com>
2016-06-09 01:15:38 +0300

    let`s go forward

---
+++ console_app/app.js
@@ -0,0 +1,33 @@
+var argv = require('minimist')(process.argv.slice(2));
+var prompt = require('prompt');
+var colors = require('colors/safe');
+
+prompt.override = argv;
+prompt.message = '';
+prompt.delimiter = colors.cyan(':');
+//prompt.colors = false;
+
+var schema = {
+    properties: {
+        name: {
+            description: colors.cyan('name'),
+            pattern: /^[a-zA-Z\s\-]+$/,
+            message: 'Name must be only letters, spaces, or dashes',
+            required: true
+        },
+        password: {
+            description: colors.cyan('password'),
+            hidden: true,
+            required: true,
+            message: 'You must write your password'
+        }
+    }
+};
+
+prompt.start();
+
+prompt.get(schema, function(error, result) {
+    console.log('Your name is: ' + result.name);
+    console.log('And your password is: ' + result.password);
+    console.log('That is all!');
+});
