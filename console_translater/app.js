3a5dc93e3f6b8823083091e048ff9ef97d55afaf
Vadym Kibanov <dev.kibanov@gmail.com>
2016-07-04 00:54:58 +0300

    Created console Translater

--- console_translater/app.js
+++ console_translater/app.js
@@ -8,7 +8,6 @@ prompt.get('t', function (err, result) {
         console.error(err);
     } else {
         var APIKey = 'trnsl.1.1.20160703T165611Z.3f81e93b1efbc536.52bb87dccc85720f151e884d8825ba920184ff6b';
-        //var text = 'Hello World';
         var text = result.t;
         var lang = 'ru';
         var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + APIKey + '&text=' + text + '&lang=' + lang;
@@ -21,7 +20,6 @@ prompt.get('t', function (err, result) {
                 } else {
                     var result = JSON.parse(body);
                     console.log(result.text);
-                    //console.log(response.statusCode);
                 }
             }
         );
